7a7e568d5ebce1382418d7d47eaa79d115ecfe74
彭云鹃 <1932358214@qq.com>
2021-02-10 22:42:33 +0800

    导航条‘

--- user/components/Header.js
+++ user/components/Header.js
@@ -1,7 +1,7 @@
 /**导航栏组件 */
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/components/header.css'
-import { Row, Col, Menu, Icon } from 'antd' //使用Ant Design的24格栅格化布局
+import { Row, Col, Menu, Icon, Button, Input } from 'antd' //使用Ant Design的24格栅格化布局
 import Router from 'next/router'
 import Link from 'next/link'
 import axios from 'axios'
@@ -10,51 +10,72 @@ import servicePath from '../config/apiUrl'
 
 const Header = () => {
     //getInitialProps不能在子组件中使用
-    const [navArray,setNavArray] = useState([])  //初始值为空
-    useEffect(()=>{
-        const fetchData = async()=>{
-            const result = await axios(servicePath.getTypeInfo)
-                        .then((res)=>{
-                            return res.data.data
-                        })
-            setNavArray(result)
-        }
-        fetchData() //让它执行
-    },[])
+    const [navArray, setNavArray] = useState([
+        { id: '0', icon: 'home', typeName: '首页' },
+        { id: '1', icon: 'read', typeName: '诗词' },
+        { id: '2', icon: 'highlight', typeName: '社区' },
+        { id: '3', icon: 'question-circle', typeName: '问答' }
+    ])  //初始值为空
+    const {Search} = Input //搜索框
 
-    const handleClickNav = (e)=>{
+    //待解决
+    // const [nowNav,setNowNav] = useState('0')
+    // useEffect(()=>{
+    //     setNowNav('2')
+    // },[])
+    // useEffect(()=>{
+    //     // const fetchData = async()=>{
+    //     //     const result = await axios(servicePath.getTypeInfo)
+    //     //                 .then((res)=>{
+    //     //                     return res.data.data
+    //     //                 })
+    //     //     setNavArray(result)
+    //     // }
+    //     // fetchData() //让它执行
+    //     setNavArray('诗词鉴赏')
+    // },[])
+
+    const handleClickNav = (e) => {
         //e可以获取Ant Design中的menu传递来的key值
-        if(e.key == 0){
+        if (e.key == 0) {
             Router.push('/')
-        }else{
-            Router.push('/list?id='+e.key)
+        } else {
+            Router.push('/list?id=' + e.key)
         }
     }
 
     return (
         <div className="header">
             <Row type="flex" justify="center">
-                <Col xs={24} sm={24} md={10} lg={15} xl={12}>
-                    <img className="header-logo" src="" alt=""/>
-                    <span className="header-txt">万一努力的尽头，是你呢</span>
+                <Col xs={2} sm={2} md={2} lg={2} xl={2}>
+                    <img className="header-logo" src="https://raw.githubusercontent.com/muAluo-Juan/react-hooks-poem/master/user/img/logo.png" alt="夜雨时" />
                 </Col>
-                <Col xs={0} sm={0} md={14} lg={8} xl={6}>
-                    <Menu mode="horizontal" onClick={handleClickNav}>
-                    <Menu.Item key="0">
-                        <Icon type="home" />
-                        首页
-                    </Menu.Item>
-                    {
-                        navArray.map((item)=>{
-                            return (
-                                <Menu.Item key={item.id}>
-                                    <Icon type={item.icon} />
-                                    {item.typeName}
-                                </Menu.Item>
-                            )
-                        })
-                    }
+                <Col xs={22} sm={22} md={8} lg={10} xl={11}>
+                    <Menu mode="horizontal" onClick={handleClickNav} defaultSelectedKeys={['0']}>
+                        {/* <Menu.Item key="0">
+                            <Icon type="read"/>
+                            诗词大全
+                        </Menu.Item> */}
+                        {
+                            navArray.map((item) => {
+                                return (
+                                    <Menu.Item key={item.id}>
+                                        <Icon type={item.icon} />
+                                        {item.typeName}
+                                    </Menu.Item>
+                                )
+                            })
+                        }
                     </Menu>
+                </Col>
+                <Col xs={0} sm={0} md={14} lg={10} xl={8}>
+                    <Search
+                        placeholder="搜索诗词"
+                        onSearch={value => console.log(value)}
+                        className="search"
+                    />
+                    <Button type="primary">登录</Button>
+                    <Button type="danger" className="btn-user">注册</Button>
                 </Col>
             </Row>
         </div>
